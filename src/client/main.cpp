b79c38caf176864d2c5e32b02c046133427ceabe
SATHIAKUMAR Thanusan <thanusan.sathiakumar@ensea.fr>
2019-10-21 11:27:31 +0200

    SFML test 4
    text + rectangle

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -5,21 +5,50 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1800, 900), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1950, 900), "SFML works!");
     sf::Texture Map;
     Map.loadFromFile("rsc/Images/level1_completeMap.png");
     
     sf::Sprite mapSprite;
     mapSprite.setTexture(Map);
-    mapSprite.setScale(1.125,1.125);
 
     sf::Texture perso;
     perso.setSmooth(true);
     perso.loadFromFile("rsc/Images/hero1.png", sf::IntRect(64, 128, 64, 64));
     sf::Sprite spritePerso;
     spritePerso.setTexture(perso, true);
-    spritePerso.setScale(1.125,1.125);
-    spritePerso.setScale(1,1);
+
+    sf::RectangleShape rectangle(sf::Vector2f(350, 800));
+    rectangle.setPosition(sf::Vector2f(1600,0));
+
+    
+    sf::Text text;
+    sf::Font font;
+
+    font.loadFromFile("rsc/Font/Game_Played.otf");
+    // choix de la chaîne de caractères à afficher
+    text.setString("Hello world");
+
+    // choix de la taille des caractères
+    text.setFont(font);
+    text.setCharacterSize(24); // exprimée en pixels, pas en points !
+    text.setPosition(sf::Vector2f(1600,0));
+    //text.setColor(sf::Color::Red);
+    text.setFillColor(sf::Color::Blue);
+
+
+
+
+    /**********************************/
+    sf::Vertex line[] =
+    {
+        sf::Vertex(sf::Vector2f(0, 10)),
+        sf::Vertex(sf::Vector2f(0, 150)),
+        sf::Vertex(sf::Vector2f(10, 150))
+
+    };
+
+    /**********************************/
 
     while (window.isOpen())
     {
@@ -33,6 +62,9 @@ int main()
         window.clear();
         window.draw(mapSprite);
         window.draw(spritePerso);
+        window.draw(line, 2, sf::Lines);
+        window.draw(rectangle);
+        window.draw(text);
         window.display();
     }
 
