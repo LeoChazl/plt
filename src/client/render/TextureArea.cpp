9889c389aecd62b35b12dd54ce512f7e8c21cb1c
LeoChazl <leo.chazallet@ensea.fr>
2019-10-22 16:47:09 +0200

    Implementation of render functions update 2.5

--- src/client/render/TextureArea.cpp
+++ src/client/render/TextureArea.cpp
@@ -12,7 +12,7 @@ bool TextureArea::loadBattleField(int mapHeight, int mapWidth , string mapImageD
      return texture.loadFromFile(mapImageDirectory);
 }
 
-bool TextureArea::loadUnits(state::State& currentState, map<int,TileSet&> textureTileset){
+bool TextureArea::loadUnits(state::State& currentState, vector<unique_ptr<TileSet>>& textureTileset){
 
     quads.setPrimitiveType(sf::Quads);
 
@@ -21,9 +21,9 @@ bool TextureArea::loadUnits(state::State& currentState, map<int,TileSet&> textur
         currentPlayer=*currentState.getPlayerList()[i];
         for(unsigned int j=0;j<currentPlayer.getMobileEntityList().size();j++){
             if(currentPlayer.getMobileEntityList()[j]->getEntityId()==TROLL){
-                texture = textureTileset.at(TROLLTILESET).getTexture();
+                texture = textureTileset[TROLLTILESET]->getTexture();
 
-                quads.resize(quads.getVertexCount() + textureTileset.at(TROLLTILESET).getCellHeight() * textureTileset.at(TROLL).getCellWidth() * 4);
+                quads.resize(quads.getVertexCount() + textureTileset[TROLLTILESET]->getCellHeight() * textureTileset[TROLLTILESET]->getCellWidth() * 4);
 
                 int tx = 1; //on ne prend que dans la première colonne pour
                 int ty = currentPlayer.getMobileEntityList()[j]->getDirection() * 64;
