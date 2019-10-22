a1bbd5abdb205a4b2363ffdf3ca3fa2be8c9c8af
LeoChazl <leo.chazallet@ensea.fr>
2019-10-22 17:38:57 +0200

    Add new files for new map implementation

---
+++ rsc/Images/WaterfallDarkBlue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="WaterfallDarkBlue" tilewidth="32" tileheight="32" tilecount="192" columns="32">
+ <image source="../../Ressources/[A]WaterFall1_pipo.png" width="1024" height="192"/>
+</tileset>
