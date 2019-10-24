14d5e2bd7c72fb435ab89d5eb085d34c985f7e5a
SATHIAKUMAR Thanusan <thanusan.sathiakumar@ensea.fr>
2019-10-24 21:15:20 +0200

    Add comments in render files

--- src/client/render/StateLayer.cpp
+++ src/client/render/StateLayer.cpp
@@ -5,7 +5,11 @@ using namespace render;
 using namespace std;
 
 
-//Constructor
+/*Constructor
+*
+*store in a TileSet pointor array all the TIleset type object pointor
+*this TileSet object containt a sf::Texture object with the specific image loaded on it
+**/
 StateLayer::StateLayer (sf::RenderWindow& window, state::State& State){
     font.loadFromFile("rsc/Font/Game_Played.otf");
     screenWidth=1950;
@@ -14,13 +18,13 @@ StateLayer::StateLayer (sf::RenderWindow& window, state::State& State){
     mapWidth=1600;
     mapHeight=800;
 
-	TileSet tileSetCharacters(UNITSTILESET);
+	TileSet tileSetCharacters(UNITSTILESET);//Load units tileset image
 	unique_ptr<TileSet> ptr_charTileSet (new TileSet(tileSetCharacters));
-	tileSets.push_back(move(ptr_charTileSet));
+	tileSets.push_back(move(ptr_charTileSet));//Store pointor of the TileSet object
 
-    TileSet tileSetCursor(CURSORTILESET);
+    TileSet tileSetCursor(CURSORTILESET);//Load Cursor tileset image
 	std::unique_ptr<TileSet> ptr_cursorTileSet (new TileSet(tileSetCursor));
-	tileSets.push_back(move(ptr_cursorTileSet));
+	tileSets.push_back(move(ptr_cursorTileSet));//Store pointor of the TileSet object
 }
 
 
@@ -32,6 +36,11 @@ StateLayer::StateLayer (sf::RenderWindow& window, state::State& State){
      return textureAreas;
  }
 
+/*initTextureAreas
+*
+*create different layer: one for map,one for units,one for cursor
+*store this layer in a array--> type TextureAreas
+**/
 void StateLayer::initTextureAreas (state::State state){
     //Déclaration des textures
     TextureArea map;
@@ -61,10 +70,14 @@ void StateLayer::initTextureAreas (state::State state){
     textureAreas.push_back(move(ptr_cursor));
 }
 
-
+/*draw
+*
+*Display each of the TextureArea layer in screen--> initialized in textureArea array with the "initTextureAreas(state)" function
+**/
 void StateLayer::draw (sf::RenderWindow& window){
     window.clear();
 
+    //Initialize rectangle Texture in the Window-->LAYER
     // Rectangle degrade en (0,400) et de taille 400x200
 	sf::VertexArray quad(sf::Quads, 4);
 	quad[0].position = sf::Vector2f(1600.f, 0.f);
@@ -76,17 +89,18 @@ void StateLayer::draw (sf::RenderWindow& window){
 	quad[2].color = sf::Color::Black;
 	quad[3].color = sf::Color::Black;
 
+    //Initialise a logo Texture in the Window-->LAYER
     sf::Texture logo;
     logo.loadFromFile("rsc/Images/fire_emblem_logo.png");
     sf::Sprite spriteLogo;
     spriteLogo.setPosition(1600,800);
     spriteLogo.setTexture(logo, true);
 
-    window.draw(spriteLogo);
-    window.draw(quad);
-	window.draw(*textureAreas[0]);	// Draw the map			
-	window.draw(*textureAreas[1]);	// Draw the units
-	window.draw(*textureAreas[2]);	// Draw the cursor
+    window.draw(spriteLogo); //DRaw the logo layer
+    window.draw(quad); //Draw the rectangle layer
+	window.draw(*textureAreas[0]);	// Draw the map layer--> With the TextureArea type object as Target --> 			
+	window.draw(*textureAreas[1]);	// Draw the units layer
+	window.draw(*textureAreas[2]);	// Draw the cursor layer
     
 	window.display();
 }
--- src/client/render/TextureArea.cpp
+++ src/client/render/TextureArea.cpp
@@ -6,22 +6,25 @@ using namespace state;
 using namespace std;
 
 // Functions
-
+/*loadBattleField
+*
+*load the map image but also quads position and texture coordonnate
+**/
 bool TextureArea::loadBattleField(int mapWidth, int mapHeight , string mapImageDirectory){
-    texture.loadFromFile(mapImageDirectory);
+    texture.loadFromFile(mapImageDirectory);//texture --> an object of sf::Texture class
 
 	quads.setPrimitiveType(sf::Quads);
-    quads.resize(4);
+    quads.resize(4);//quads--> a vertex array with 4 vertex 
 
-	sf::Vertex* quad = &quads[0];
+	sf::Vertex* quad = &quads[0]; //pointor to the first vertex element of quads
 		
-	//Définition quatres coins du vertex dans la fenêtre
+	//	Definition of the four corners of the vertex in the window
 	quad[0].position = sf::Vector2f(0, 0);
 	quad[1].position = sf::Vector2f(mapWidth, 0);
 	quad[2].position = sf::Vector2f(mapWidth, mapHeight);
 	quad[3].position = sf::Vector2f(0, mapHeight);
 		
-    //Définition des coordonnées pour récupérer la première texture
+    //Definition of coordinates to retrieve the first texture
 	quad[0].texCoords = sf::Vector2f(0, 0);
 	quad[1].texCoords = sf::Vector2f(mapWidth, 0);
 	quad[2].texCoords = sf::Vector2f(mapWidth, mapHeight);
@@ -30,19 +33,26 @@ bool TextureArea::loadBattleField(int mapWidth, int mapHeight , string mapImageD
 	return true;
 }
 
+/*loadUnits
+*
+*load the tilesets recover from tileSet Class
+*load the quads associated to each units belonging to each player
+**/
 bool TextureArea::loadUnits(state::State& currentState, TileSet& textureTileset){
-    texture = textureTileset.getTexture();
+    texture = textureTileset.getTexture(); //Recover the sf::Texture with TileSet image loaded initialised in render::TileSet Class
 
     quads.setPrimitiveType(sf::Quads);
 
-    Player currentPlayer;
+    Player currentPlayer;//Initialise Player class object in order to have the list of units belonging to each player
     int vertexArrayIndex = 0;
 
+    //for each player initialized in the state object 
     for(unsigned int i=0;i<currentState.getPlayerList().size();i++){
-        currentPlayer=*currentState.getPlayerList()[i];
+        currentPlayer=*currentState.getPlayerList()[i];//take the value of one player pointor adn store it in Player type variable
         cout << "Player id: " << currentPlayer.getId() << endl;
         cout << "Number of units: " << currentPlayer.getMobileEntityList().size() << endl;
 
+        //for each units controlled by the player choose before 
         for(unsigned int j=0;j<currentPlayer.getMobileEntityList().size();j++){
             quads.resize(quads.getVertexCount() + 4);
             sf::Vertex* quad = &quads[vertexArrayIndex * 4];  
@@ -51,13 +61,16 @@ bool TextureArea::loadUnits(state::State& currentState, TileSet& textureTileset)
             int tx = currentPlayer.getMobileEntityList()[j]->getEntityId();
             int ty = currentPlayer.getMobileEntityList()[j]->getDirection();
 
-            //Définition quatres coins du vertex dans la fenêtre
+            //Definition of the four corners of the vertex in the window
+               //2 coordinate(x,y) for each vertx--> x = the unit X position in the map product the width of the unit tileSet
+               //                                --> y = the unit Y position in the map product the height of the unit tileSet
             quad[0].position = sf::Vector2f(currentPlayer.getMobileEntityList()[j]->getX() * textureTileset.getCellWidth(),currentPlayer.getMobileEntityList()[j]->getY() * textureTileset.getCellHeight());
             quad[1].position = sf::Vector2f((currentPlayer.getMobileEntityList()[j]->getX()+1) * textureTileset.getCellWidth(),currentPlayer.getMobileEntityList()[j]->getY() * textureTileset.getCellHeight());
             quad[2].position = sf::Vector2f((currentPlayer.getMobileEntityList()[j]->getX()+1) * textureTileset.getCellWidth(),(currentPlayer.getMobileEntityList()[j]->getY()+1) * textureTileset.getCellHeight());
             quad[3].position = sf::Vector2f(currentPlayer.getMobileEntityList()[j]->getX() * textureTileset.getCellWidth(),(currentPlayer.getMobileEntityList()[j]->getY()+1) * textureTileset.getCellHeight());
             
-            //Définition des coordonnées pour récupérer la bonne texture
+            //Definition of coordinates to retrieve the first texture
+               //same as quads position
             quad[0].texCoords = sf::Vector2f(tx * textureTileset.getCellWidth(), ty * textureTileset.getCellHeight());
             quad[1].texCoords = sf::Vector2f((tx+1) * textureTileset.getCellWidth(), ty * textureTileset.getCellHeight());
             quad[2].texCoords = sf::Vector2f((tx+1) * textureTileset.getCellWidth(), (ty+1) * textureTileset.getCellHeight());
