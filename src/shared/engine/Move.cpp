356bee5a50dabcb91e6589094dcc465dd73b37d0
LeoChazl <leo.chazallet@ensea.fr>
2019-11-05 16:09:42 +0100

    Implementation of engine 1

---
+++ src/shared/engine/Move.cpp
@@ -0,0 +1,15 @@
+#include "../engine.h"
+
+using namespace engine;
+
+// Constructor
+
+Move::Move(state::MobileEntity& target, state::Position& destination){
+    id=MOVE;
+}
+
+// Function
+
+void Move::Move(state::State& state){
+
+}
