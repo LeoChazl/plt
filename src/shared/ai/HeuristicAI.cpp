a5bb33cf2a147f7af138c47e1b11ecc7ebebdaf4
LeoChazl <leo.chazallet@ensea.fr>
2019-11-27 10:34:52 +0100

    Ai dia update

---
+++ src/shared/ai/HeuristicAI.cpp
@@ -0,0 +1,13 @@
+#include "../engine.h"
+#include "../state.h"
+#include "../ai.h"
+#include <iostream>
+#include <unistd.h>
+
+using namespace state;
+using namespace engine;
+using namespace std;
+using namespace ai;
+
+// Functions 
+
