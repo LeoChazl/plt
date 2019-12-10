deaeb2cbff4a0eb1af891d172ff7c6ecf52e6db2
SATHIAKUMAR Thanusan <thanusan.sathiakumar@ensea.fr>
2019-12-10 01:34:00 +0100

    Deep AI :find and store all possible situation for the first depth

--- src/shared/ai/DeepAI.cpp
+++ src/shared/ai/DeepAI.cpp
@@ -32,3 +32,75 @@ void DeepAI::copyEngine (engine::Engine& engine,engine::Engine& copiedEngine){
     copiedEngine.setStop(engine.getStop());
 }
 
+
+/**
+ * 
+ */
+std::vector<state::Position> DeepAI::attackableEnemies (engine::Engine& copiedEngine, int aiUnitIndex, int artificalIntelligenceID){
+    std::vector<state::Position> attackableEnemiesPositionList;
+    std::vector<state::Position> allowedAttackPosition=copiedEngine.getState().getPlayerList()[artificalIntelligenceID]->getMobileEntityList()[aiUnitIndex]->allowedAttack(copiedEngine.getState());
+    int x,y;
+    for (int i = 0; i < allowedAttackPosition.size(); i++)
+    {
+        x=allowedAttackPosition[i].getX();
+        y=allowedAttackPosition[i].getY();
+        if(copiedEngine.getState().isOccupied(x,y)){
+            attackableEnemiesPositionList.push_back(allowedAttackPosition[i]);
+        }
+    }
+    return attackableEnemiesPositionList;
+    
+}
+
+
+/**
+ * 
+ */ 
+std::vector<state::Position> DeepAI::optimalMoveCoord (engine::Engine& copiedEngine, int aiUnitIndex, int artificialIntelligenceID){
+    //change with the ASTAR Algorithm results
+    std::vector<state::Position> optimalMoveCoord=copiedEngine.getState().getPlayerList()[artificialIntelligenceID]->getMobileEntityList()[aiUnitIndex]->allowedMove(copiedEngine.getState());
+    return optimalMoveCoord;
+}
+
+
+/**store all possible attack commands in a list
+ * 
+ *  No Return
+ */
+void DeepAI::storeAttackCommands (engine::Engine& engine, std::vector<state::Position>& attackableEnemiesPositionList, std::vector<std::shared_ptr<engine::Command>>& possibleCommandList, int aiUnitIndex){
+    int x,y;
+    for (int i = 0; i < attackableEnemiesPositionList.size(); i++)
+    {
+        x=attackableEnemiesPositionList[i].getX();
+        y=attackableEnemiesPositionList[i].getY();
+        Attack attack(*engine.getState().ptrReferenceToPlayingEntity(aiUnitIndex), *engine.getState().getMobileEntity(x,y)); //Initialize the Attack constrctor with attacker and target
+		shared_ptr<Command> ptr_attack(new Attack(attack));
+        possibleCommandList.push_back(ptr_attack);
+    }
+}
+
+/**store all possible move commands in the possible command list
+ * 
+ *  No return
+ */
+void DeepAI::storeMoveCommands (engine::Engine& copiedEngine, std::vector<state::Position>& optimalMoveCoord, std::vector<std::shared_ptr<engine::Command>>& possibleCommandList, int aiUnitIndex){
+    
+    for (int i = 0; i < optimalMoveCoord.size(); i++)
+    {
+        Move movement(*copiedEngine.getState().ptrReferenceToPlayingEntity(aiUnitIndex), optimalMoveCoord[i]);//Initialize the movement pointor with the unit to move and the position to reach
+		shared_ptr<Command> ptr_move(new Move(movement));
+        possibleCommandList.push_back(ptr_move);
+    }
+}
+
+/**store End Action command in the possible command list
+ * 
+ *  No Return
+ */
+void DeepAI::storeEndActionCommand(engine::Engine& copiedEngine, std::vector<std::shared_ptr<engine::Command>>& possibleCommandList, int aiUnitIndex){
+    EndEntityRound endEntityRound(*copiedEngine.getState().ptrReferenceToPlayingEntity(aiUnitIndex));
+	shared_ptr<Command> ptr_endEntityRound(new EndEntityRound(endEntityRound));
+    possibleCommandList.push_back(ptr_endEntityRound);
+}
+
+
