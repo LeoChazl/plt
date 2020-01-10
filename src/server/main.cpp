7a010a55e089cd078f437c3f8608e162a179a13d
LeoChazl <leo.chazallet@ensea.fr>
2020-01-10 17:17:05 +0100

    API Implementation

---
+++ src/server/main.cpp
@@ -0,0 +1,123 @@
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <microhttpd.h>
+#include <cstring>
+#include "server.h"
+
+
+using namespace sf;
+using namespace std;
+using namespace server;
+
+class Request {
+public:
+    struct MHD_PostProcessor *pp = nullptr;
+    string data;
+    ~Request() {
+        if (pp) MHD_destroy_post_processor (pp);
+    }
+};
+
+// Function handling important uploaded data (not used here)
+static int
+post_iterator(void *cls,
+        enum MHD_ValueKind kind,
+        const char *key,
+        const char *filename,
+        const char *content_type,
+        const char *transfer_encoding,
+        const char *data, uint64_t off, size_t size) 
+{
+    return MHD_NO;
+}
+
+// Destroy the data of a request
+static void
+request_completed (void *cls, struct MHD_Connection *connection,
+                   void **con_cls, enum MHD_RequestTerminationCode toe)
+{
+  Request *request = (Request*)*con_cls;
+  if (request) {
+      delete request;
+      *con_cls = nullptr;
+  }
+}
+
+// Main handler
+static int
+main_handler (void *cls,      
+          struct MHD_Connection *connection,
+          const char *url, // 
+          const char *method,
+          const char *version,
+          const char *upload_data, size_t *upload_data_size, void **ptr) 
+{
+    // Données pour une requête (en plusieurs appels à cette fonction)
+    Request *request = (Request*)*ptr;
+
+    // Premier appel pour cette requête
+    if (!request) { 
+        request = new Request();
+        if (!request) {
+            return MHD_NO;
+        }
+        *ptr = request;
+
+        return MHD_YES;
+    }    
+    
+    // Cas où il faut récupérer les données envoyés par l'utilisateur
+    if (strcmp(method, MHD_HTTP_METHOD_POST) == 0
+     || strcmp(method, MHD_HTTP_METHOD_PUT) == 0) {
+        if (*upload_data_size != 0) {
+            request->data = upload_data;
+            *upload_data_size = 0;
+            return MHD_YES;
+        }    
+    }
+
+    HttpStatus status;
+    string response;
+    try {
+
+        ServicesManager *manager = (ServicesManager*) cls;
+        status = manager->queryService(response,request->data,url,method);
+    }
+    catch(ServiceException& e) {
+        status = e.status();
+        response = e.what();
+        response += "\n";
+    }
+    catch(exception& e) {
+        status = HttpStatus::SERVER_ERROR;
+        response = e.what();
+        response += "\n";
+    }
+    catch(...) {
+        status = HttpStatus::SERVER_ERROR;
+        response = "Unknown exception\n";
+    }
+
+    struct MHD_Response *mhd_response;
+    mhd_response = MHD_create_response_from_buffer(response.size(),(void *)response.c_str(),MHD_RESPMEM_MUST_COPY);
+    if (strcmp(method,MHD_HTTP_METHOD_GET) == 0) {
+        MHD_add_response_header(mhd_response,"Content-Type","application/json; charset=utf-8");
+    }
+    int ret = MHD_queue_response(connection, status, mhd_response);
+    MHD_destroy_response(mhd_response);
+    return ret;
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc>1){
+        if(strcmp(argv[1],"listen")==0){
+            cout << "I am the server" << endl;
+        }
+    }
+    return 0;
+}
+
+
